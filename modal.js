1caa067f6ba02611f9e14547b700b82caa21a4ac
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-04 13:25:12 +0200

    added background color on the background Img on click

--- modal.js
+++ modal.js
@@ -5,10 +5,22 @@ let modalOverlay = document.querySelector(".modal-overlay");
 openModal.addEventListener("click", function(){
     modalOverlay.style.visibility = "visible";
     modalOverlay.style.zIndex = "10";
-
+    document.body.appendChild(overlay);
 })
 
 closeBtn.addEventListener("click", function(){
     modalOverlay.style.visibility = "hidden";
     modalOverlay.style.zIndex = "-10";
+    document.body.removeChild(overlay);
 })
+
+let overlay = document.createElement("div");
+
+overlay.style.position = 'fixed';
+overlay.style.top = '0';
+overlay.style.left = '0';
+overlay.style.width = '100%';
+overlay.style.height = '100%';
+overlay.style.backgroundColor = 'rgba(0, 0, 255, 0.3)'; 
+overlay.style.zIndex = '1';
+
